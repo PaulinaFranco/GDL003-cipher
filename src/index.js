4701bda981e15b6b49271b28fd5ba38eb92f08b3
pahua <dadiante@gmail.com>
2019-06-07 00:06:01 -0500

    activando botones, declarando variables y testeos

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
-//LLAMANDO BOTONES PARA CIFRAR Y DESCIFRAR EN INTRO
-document.getElementById("encodeButton").addEventListener("click", encodeFunction);
-document.getElementById("decodeButton").addEventListener("click", decodeFunction);
+/// LLAMANDO BOTONES
 
-//FUNCIÓN CIFRAR
-function encodeFunction() {
-  alert = "check";
-  //MOSTRANDO SECCIÓN DE RESULTADOS
-  document.getElementById("result").style.display = "block";
-  //OCULTANDO SECCIÓN INTRO
+document.getElementById("encodeButton").addEventListener("click", showProduct);
+document.getElementById("decodeButton").addEventListener("click", showProduct);
+let textToProcess = document.getElementById("text");
+let valueOffset = document.getElementById("offsetSelector");
+/// TODAVÍA NO RESUELVO QUÉ VA  A HACER EL BOTÓN DE PÁNICO
+document.getElementById("panicButton").addEventListener("click", function(){
+  alert (textToProcess.value + valueOffset.value);
+  let valueText = textToProcess.value;
+    console.log(window.cipher.decode(valueText));
+});
+///FALTA ACTIVAR BOTÓN DE REGRESO -resetButton
+
+//tengo problemas para mostrar elemento product
+function showProduct (){
   document.getElementById("entry").style.display = "none";
-};
-
-
-//FUNCIÓN DESCIFRAR
-function decodeFunction() {
-  alert = "check";
-  //MOSTRANDO SECCIÓN DE RESULTADOS
-  document.getElementById("result").style.display = "block";
-  //OCULTANDO SECCIÓN INTRO
-  document.getElementById("entry").style.display = "none";
-};
+    document.getElementById("product").style.visibility = "visible";
+}
