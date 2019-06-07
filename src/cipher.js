06cfad14d88cc005f9488321dc49319e5763f6ec
pahua <dadiante@gmail.com>
2019-06-07 00:03:30 -0500

    empezando objeto encode, probando .split y map

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,22 @@
+//ARCHIVO PARA LAS FUNCIONES
+
 window.cipher = {
-  // ... 
+  decode: function(textToProcess) {
+    console.log(textToProcess);
+    let textSplit = textToProcess.split("");
+    console.log(textSplit);
+    // arrow function const funcion = () =>
+    let check = textSplit.map((letra) => {
+      return letra.charCodeAt();
+    })
+    console.log(check)
+  }
+  // AQUÍ UN OBJETO QUE CONSTA DE PROPIEDAD Y SU VALOR...
+
+/*  //AÚN NO ENTIENDO ESTA FÓRMULA
+function decode("textToProcess") {
+  let textSplit = textToProcess.split(" ");
+  let check = textSplit.map(1 => 1.charCodeAt());
+}*/
+  //string? array? y offset serían los parámetro
 };
