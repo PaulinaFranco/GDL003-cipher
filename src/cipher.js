5b63898a6b1c84303631bb939067ad9f659f42b1
pahua <dadiante@gmail.com>
2019-06-10 23:41:02 -0500

    cifrado y descifrado básico: mayúsculas dentro de rango y sin espacios

--- src/cipher.js
+++ src/cipher.js
@@ -1,10 +1,9 @@
 //ARCHIVO PARA LAS FUNCIONES
 
 window.cipher = {
-  /*
-  encode: function(textToProcess) {
-            console.log(textToProcess);
-            let textSplit = textToProcess.split("");
+    encode: function(textToProcess) {
+            console.log(textToProcess.value);
+            let textSplit = textToProcess.value.split("");
             console.log(textSplit);
     // arrow function const funcion = () =>*
             let valueOffset = document.getElementById("offsetSelector");
@@ -14,20 +13,23 @@ window.cipher = {
             console.log(almostEncoded);
             console.log(typeof almostEncoded);
             console.log(Array.isArray(almostEncoded));
-            return almostEncoded;
-          }, */
+            return almostEncoded.join("");
+          },
+
 
  decode: function(textToProcess) {
-        console.log(textToProcess);
-        let almostDecoded = textToProcess.split("");
-        almostDecoded= String.fromCharCode(almostDecoded);
+        console.log(textToProcess.value);
+        let textSplit = textToProcess.value.split("");
+        console.log(textSplit);
+        let valueOffset = document.getElementById("offsetSelector");
+        let almostDecoded = textSplit.map((x) =>
+        String.fromCharCode(x.charCodeAt() - valueOffset.value %26));
         console.log(almostDecoded);
-
-
+        return almostDecoded.join("");
+}
+};
           // arrow function const funcion = () =>*
           //  let valueOffset = document.getElementById("offsetSelector");
-            }
-          };
 
 /*  //AÚN NO ENTIENDO ESTA FÓRMULA
 function decode("textToProcess") {
