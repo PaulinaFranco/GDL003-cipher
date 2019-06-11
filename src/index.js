080264a20a520bb7077b9f37c7ccf4082e79b802
pahua <dadiante@gmail.com>
2019-06-10 23:43:01 -0500

    vinculación de botones cifrado y descifrado con contenido de textareas de pantalla de resultado

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
 /// LLAMANDO BOTONES
 
-document.getElementById("encodeButton").addEventListener("click", showProduct);
-document.getElementById("decodeButton").addEventListener("click", showProduct);
+document.getElementById("encodeButton").addEventListener("click", showEncode);
+document.getElementById("decodeButton").addEventListener("click", showDecode);
 let textToProcess = document.getElementById("text");
 let valueOffset = document.getElementById("offsetSelector");
 /// TODAVÍA NO RESUELVO QUÉ VA  A HACER EL BOTÓN DE PÁNICO
 document.getElementById("panicButton").addEventListener("click", function(){
   alert (textToProcess.value + valueOffset.value);
-  let valueText = textToProcess.value;
-    console.log(window.cipher.decode(valueText));
+let valueText = textToProcess.value;
+console.log(window.cipher.encode (textToProcess));
 });
 ///FALTA ACTIVAR BOTÓN DE REGRESO -resetButton
 
+
 //tengo problemas para mostrar elemento product
-function showProduct (){
+function showEncode (){
   document.getElementById("entry").style.display = "none";
-    document.getElementById("product").style.visibility = "visible";
-}
+  document.getElementById("product").style.visibility = "visible";
+  document.getElementById("originalText").value= textToProcess.value;
+  document.getElementById("resultText").value = window.cipher.encode (textToProcess);
+};
+
+function showDecode (){
+  document.getElementById("entry").style.display = "none";
+  document.getElementById("product").style.visibility = "visible";
+  document.getElementById("originalText").value= textToProcess.value;
+  document.getElementById("resultText").value = window.cipher.decode (textToProcess);
+};
