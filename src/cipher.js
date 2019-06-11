a0941d53fc8900df196986f42ad2c1a324c46d81
pahua <dadiante@gmail.com>
2019-06-10 21:51:03 -0500

    versión con pérdida de avances, se guarda para comparar

--- src/cipher.js
+++ src/cipher.js
@@ -1,20 +1,33 @@
 //ARCHIVO PARA LAS FUNCIONES
 
 window.cipher = {
-  encoding: function(textToProcess) {
-    ///
-    console.log(textToProcess);
-    const textSplit = textToProcess.split("");
-    ///
-    console.log(textSplit);
+  /*
+  encode: function(textToProcess) {
+            console.log(textToProcess);
+            let textSplit = textToProcess.split("");
+            console.log(textSplit);
     // arrow function const funcion = () =>*
-    let letters = textSplit.map((x) => {
-    return x.charCodeAt();
-    })
-    ///
-  console.log(letters);
-  }
-  // AQUÍ UN OBJETO QUE CONSTA DE PROPIEDAD Y SU VALOR...
+            let valueOffset = document.getElementById("offsetSelector");
+            let almostEncoded = textSplit.map((x) =>
+            String.fromCharCode(x.charCodeAt() + valueOffset.value %26));
+
+            console.log(almostEncoded);
+            console.log(typeof almostEncoded);
+            console.log(Array.isArray(almostEncoded));
+            return almostEncoded;
+          }, */
+
+ decode: function(textToProcess) {
+        console.log(textToProcess);
+        let almostDecoded = textToProcess.split("");
+        almostDecoded= String.fromCharCode(almostDecoded);
+        console.log(almostDecoded);
+
+
+          // arrow function const funcion = () =>*
+          //  let valueOffset = document.getElementById("offsetSelector");
+            }
+          };
 
 /*  //AÚN NO ENTIENDO ESTA FÓRMULA
 function decode("textToProcess") {
@@ -22,4 +35,3 @@ function decode("textToProcess") {
   let check = textSplit.map(1 => 1.charCodeAt());
 }*/
   //string? array? y offset serían los parámetro
-};
