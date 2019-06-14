91d3f93d4bd8382b82ad6d14d95b3ce00186a07a
pahua <dadiante@gmail.com>
2019-06-14 13:29:20 -0500

    se trata de acomodar el código para que esté más ordenado

--- src/index.js
+++ src/index.js
@@ -1,34 +1,33 @@
-/// LLAMANDO BOTONES
 
-document.getElementById("encodeButton").addEventListener("click", showEncode);
-document.getElementById("decodeButton").addEventListener("click", showDecode);
 let textToProcess = document.getElementById("text");
 let valueOffset = document.getElementById("offsetSelector");
-/// TODAVÍA NO RESUELVO QUÉ VA  A HACER EL BOTÓN DE PÁNICO
-document.getElementById("panicButton").addEventListener("click", function(){
-  alert (textToProcess.value + valueOffset.value);
-let valueText = textToProcess.value;
-console.log(window.cipher.encode (textToProcess));
-});
 
-document.getElementById("resetButton").addEventListener("click", showEntry);
-function showEntry (){
+/// TODAVÍA NO RESUELVO QUÉ VA  A HACER EL BOTÓN DE Panic
+let getOut = () => {alert("enlazar a una página o un juego");
+};
+document.getElementById("panicButton").addEventListener("click", getOut);
+
+let showEntry = () => {
   document.getElementById("product").style.display = "none";
   document.getElementById("entry").style.display= "block";
-  
+  document.getElementById("text").value= null;
+  document.getElementById("offsetSelector").value= null;
 };
+document.getElementById("resetButton").addEventListener("click", showEntry);
+//  document.getElementById("originalText")=  MARCAR CONTENIDO DE INICIO VACIO TAMBIÉN PARA resultText
 
-//tengo problemas para mostrar elemento product
-function showEncode (){
+let showEncode = () => {
   document.getElementById("entry").style.display = "none";
   document.getElementById("product").style.visibility = "visible";
   document.getElementById("originalText").value= textToProcess.value;
-  document.getElementById("resultText").value = window.cipher.encode (textToProcess);
+  document.getElementById("resultText").value = window.cipher.encode(valueOffset, textToProcess);
 };
+document.getElementById("encodeButton").addEventListener("click", showEncode);
 
-function showDecode (){
+let showDecode = () => {
   document.getElementById("entry").style.display = "none";
   document.getElementById("product").style.visibility = "visible";
   document.getElementById("originalText").value= textToProcess.value;
-  document.getElementById("resultText").value = window.cipher.decode (textToProcess);
+  document.getElementById("resultText").value = window.cipher.decode(valueOffset, textToProcess);
 };
+document.getElementById("decodeButton").addEventListener("click", showDecode);
